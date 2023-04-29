8a213a14aec4c801894cb6c7aecd725869621fb4
Tanaka Muchemwa <muchemwatanaka99@gmail.com>
2023-04-29 13:20:15 -0400

    server

--- server/routes.js
+++ server/routes.js
@@ -2,67 +2,68 @@ const mysql = require('mysql')
 const config = require('./config.json')
 
 const connection = mysql.createConnection({
-    host: config.rds_host,
-    user: config.rds_user,
-    password: config.rds_password,
-    port: config.rds_port,
-    database: config.rds_db
-  });
-  connection.connect((err) => err && console.log(err));
+  host: config.rds_host,
+  user: config.rds_user,
+  password: config.rds_password,
+  port: config.rds_port,
+  database: config.rds_db,
+})
+connection.connect((err) => err && console.log(err))
 
-  /******************
+/******************
  *ROUTES BELOW*
  ******************/
 
- // Route 1: GET /user/:user-id
-const user= async function(req, res) {
+// Route 1: GET /user/:user-id
+const user = async function (req, res) {
   //Given a user_id, returns all information about the user
-  const id = req.params.user_id;
+  const id = req.params.user_id
 
-  connection.query(`
+  connection.query(
+    `
   SELECT *
   FROM User
-  WHERE user_id = "${id}"`, (err, data) => {
-    if (err || data.length === 0) {
-      console.log(err);
-      res.json({});
-    } else {
-      
-      res.json(
-        {
+  WHERE user_id = "${id}"`,
+    (err, data) => {
+      if (err || data.length === 0) {
+        console.log(err)
+        res.json({})
+      } else {
+        res.json({
           user_id: data[0].user_id,
           name: data[0].name,
           review_count: data[0].review_count,
           yelping_since: data[0].yelping_since,
           useful: data[0].useful,
-          funny:data[0].funny,
-          cool:data[0].cool,
-          fans:data[0].fans,
+          funny: data[0].funny,
+          cool: data[0].cool,
+          fans: data[0].fans,
           elite: data[0].elite,
           average_stars: data[0].average_stars,
-          friends:data[0].friends,
-
-        });
-    }
-  });
+          friends: data[0].friends,
+        })
+      }
+    },
+  )
 }
 
- // Route 1: GET /business/:business-id
- const business= async function(req, res) {
+// Route 1: GET /business/:business-id
+const business = async function (req, res) {
   //Given a user_id, returns all information about the user
-  const id = req.params.user_id;
+  const id = req.params.user_id
 
-  connection.query(`
+  connection.query(
+    `
   SELECT *
   FROM Business
-  WHERE business_id = "${id}"`, (err, data) => {
-    if (err || data.length === 0) {
-      console.log(err);
-      res.json({});
-    } else {
-      console.log(data[0])
-      res.json(
-        {
+  WHERE business_id = "${id}"`,
+    (err, data) => {
+      if (err || data.length === 0) {
+        console.log(err)
+        res.json({})
+      } else {
+        console.log(data[0])
+        res.json({
           name: data[0].name,
           address: data[0].address,
           city: data[0].city,
@@ -75,26 +76,32 @@ const user= async function(req, res) {
           is_open: data[0].is_open,
           business_id: data[0].business_id,
           categories: data[0].categories,
-        });
-    }
-  });
+        })
+      }
+    },
+  )
 }
 
 //GET /businesses
-const businesses = async function(req, res) {
-  connection.query(`
+const businesses = async function (req, res) {
+  const page = req.query.page
+  const pageSize = req.query.page_size
+  const offset = (page - 1) * pageSize
+  connection.query(
+    `
   SELECT *
   FROM Business
-  LIMIT 27`, (err, data) => {
-    if (err || data.length === 0) {
-      console.log(err);
-      res.json([]);
-    } else {
-      console.log("query executed")
-      console.log(data.length)
-      res.json(data);
-    }
-  });
+  LIMIT ${pageSize}
+  OFFSET ${offset};`,
+    (err, data) => {
+      if (err || data.length === 0) {
+        console.log(err)
+        res.json([])
+      } else {
+        res.json(data)
+      }
+    },
+  )
 }
 
 module.exports = {
--- server/server.js
+++ server/server.js
@@ -1,21 +1,25 @@
-const express = require('express');
+const express = require('express')
 //const bodyParser = require('body-parser');
-const cors = require('cors');
-const config = require('./config');
-const routes = require('./routes');
+const cors = require('cors')
+const config = require('./config')
+const routes = require('./routes')
 
-const app = express();
-app.use(cors({
-  origin: '*',
-}));
+const app = express()
+app.use(
+  cors({
+    origin: '*',
+  }),
+)
 
 // Routes here
-app.get('/user/:user_id', routes.user);
-app.get('/business/:business_id', routes.business);
-app.get('/businesses', routes.businesses);
+app.get('/user/:user_id', routes.user)
+app.get('/business/:business_id', routes.business)
+app.get('/businesses', routes.businesses)
 
 app.listen(config.server_port, () => {
-    console.log(`Server running at http://${config.server_host}:${config.server_port}/`)
-  });
-  
-  module.exports = app;
+  console.log(
+    `Server running at http://${config.server_host}:${config.server_port}/`,
+  )
+})
+
+module.exports = app
