34a23f61e25a07d039f2a5ff86b6f5fce3ac84d0
Tanaka Muchemwa <muchemwatanaka99@gmail.com>
2023-04-30 21:50:41 -0400

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import BusinessesPage from './pages/Businesses'
-<<<<<<< HEAD
-import Navbar from './components/NavBar'
-import OneBusinessPage from './pages/BusinessInfo.js'
-=======
 import UsersPage from './pages/Users'
->>>>>>> origin/users-selassie
 
 function App() {
   return (
@@ -16,11 +11,8 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/businesses" element={<BusinessesPage />} />
-<<<<<<< HEAD
           <Route path="/business/:business_id" element={<OneBusinessPage />} />
-=======
           <Route path="/users" element={<UsersPage />} />
->>>>>>> origin/users-selassie
         </Routes>
       </BrowserRouter>
     </div>
